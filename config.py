1172bec28ebddddcf0d15bcfcf59b2a42f3d5226
James Moffet <jimmoffet@gmail.com>
2022-03-12 19:07:29 -0800

    env

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ Created on Wed Jan 29 10:29:55 2020
 @author: vlad
 """
 
-#insert local parameters
+# insert local parameters
 
-username = 'my-spotify-username'
-client_id ='my-client-id'
-client_secret = 'your-client-secret'
-redirect_uri = 'http://localhost:7777/callback'
-scope = 'user-read-recently-played'
+username = "my-spotify-username"
+client_id = "my-client-id"
+client_secret = "my-client-secret"
+redirect_uri = "http://localhost:7777/callback"
+scope = "user-read-recently-played"
